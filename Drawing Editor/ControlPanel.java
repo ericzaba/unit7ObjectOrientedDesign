eb082bc64959ef0a89674e40da42d09ff5e8f64e
ericzaba <eazaba@NN123-HA-23.HS.sd203.org>
2016-02-24 15:09:43 -0600

    drawign editor

--- Drawing Editor/ControlPanel.java
+++ Drawing Editor/ControlPanel.java
@@ -1,7 +1,46 @@
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Dimension;
 public class ControlPanel extends JPanel
 {
-   
+
+    private JPanel panel;
+    private JButton button;
+    private JLabel label;
+    
+    public ControlPanel()
+    {
+
+        this.panel = new JPanel();
+
+        
+        this.button = new JButton();
+        this.button.setPreferredSize(new Dimension(40, 40));
+        this.add(button);
+        this.label = new JLabel("Sample Text");
+        this.add(label);
+        
+        ClickListener listener = new ClickListener();
+        this.button.addActionListener(listener);
+
+    }
+    public class ClickListener implements ActionListener
+    {
+        private String name;
+
+        public void actionPerformed(ActionEvent event)
+        {
+            System.out.println("button" + event.getActionCommand() + "was clicked");
+        }
+
+    }
 
 }
--- Drawing Editor/DrawingEditor.java
+++ Drawing Editor/DrawingEditor.java
@@ -1,21 +1,40 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JFrame;
 public class DrawingEditor extends JFrame
 {
 
     private static final int FRAME_WIDTH = 400;
     private static final int FRAME_HEIGHT = 400;
-
+    private JFrame frame;
+    
     public DrawingEditor()
     {
+        
+        
         this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-        this.setTitle("Triangle");
+        this.setTitle("Drawing Editor");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JFrame frame = new JFrame();
+        ControlPanel control = new ControlPanel();
+        frame.add(control);
+        
+        
         this.setVisible(true);
     }
     
     public static void main(String[] args)
-    {
-        JFrame frame = new DrawingEditor();
-      
+    {   
+        DrawingEditor drawingeditor = new DrawingEditor();
+
+        
     }
 }
