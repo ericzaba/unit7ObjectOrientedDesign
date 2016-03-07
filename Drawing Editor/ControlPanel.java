875e779f0984da78f0232190fab39e20d4ceb331
Eric <eric.zaba@gmail.com>
2016-03-06 20:28:47 -0600

    not finished

--- Drawing Editor/ControlPanel.java
+++ Drawing Editor/ControlPanel.java
@@ -26,39 +26,72 @@ public class ControlPanel extends JPanel
     {
 
         this.panel = new JPanel();
-
+        //Code for color picker
         this.color = new JButton();
         this.color.setPreferredSize(new Dimension(40, 40));
         this.add(color);
         this.colorLabel = new JLabel("Pick Color");
         this.add(colorLabel);
 
-        
+        //Code for Circle
         this.addCircle = new JButton();
         this.addCircle.setPreferredSize(new Dimension(40, 40));
         this.add(addCircle, BorderLayout.PAGE_END);
         this.circleLabel = new JLabel("Add Circle");
         this.add(circleLabel, BorderLayout.PAGE_END);
+        this.addCircle.addActionListener(new addCircleListener());
 
+        
+        //Code for Square
         this.addSquare = new JButton();
         this.addSquare.setPreferredSize(new Dimension(40, 40));
         this.add(addSquare, BorderLayout.PAGE_END);
-
         this.squareLabel = new JLabel("Add Square");
         this.add(squareLabel, BorderLayout.PAGE_END);
+        this.addSquare.addActionListener(new addSquareListener());
     }
-    public class ClickListener implements ActionListener
+    public class addSquareListener implements ActionListener
     {
-        public ClickListener()
+        public addSquareListener()
+        {
+            addSquare.addActionListener(this);
+        }
+
+        public void actionPerformed(ActionEvent event)
+        {
+            DrawingPanel d = new DrawingPanel();
+            d.addSquare();
+        }
+    }
+    public class addCircleListener implements ActionListener
+    {
+        public addCircleListener()
         {
             addCircle.addActionListener(this);
-            addSquare.addActionListener(this);
+            
+        }
+
+        public void actionPerformed(ActionEvent event)
+        {
+            DrawingPanel d = new DrawingPanel();
+            d.addCircle();
+            
+        }
+    }
+    public class pickColorListener implements ActionListener
+    {
+        public pickColorListener()
+        {
+            color.addActionListener(this);
 
         }
 
         public void actionPerformed(ActionEvent event)
         {
+            DrawingPanel d = new DrawingPanel();
+            d.pickColor();
             
+
             
         }
     }
